b282e9668cdae62c4cc2e889f774125f26135354
Miltos Stamos <mstamos@gmail.com>
2019-02-17 12:26:49 +0200

    Moves server in separate folder and put nginx infront

--- server/api/index.js
+++ server/api/index.js
@@ -4,9 +4,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const router = require('./router');
 const app = new Koa();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(bodyParser());
 
